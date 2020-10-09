dd96f1d865b5353cf10fc810fe96f3f68be699b0
Bahaa Aldeen <Bahaa Aldeen@DESKTOP-5GJDND0>
2020-10-09 10:01:29 +0300

    Initial commit. Spring Initializr project contain : 1 - Web 2 - Jpa 3 - H2

---
+++ src/main/java/com/SpringBootQuiz/SpringBootQuiz/SpringBootQuizApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootQuiz.SpringBootQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuizApplication.class, args);
+	}
+
+}
